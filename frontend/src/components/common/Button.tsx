4e72cd5a97b98c85f62a3afe2dcb0daf1915566b
Meharp9 <kaurmeharpreer09@gmail.com>
2026-03-13 00:22:01 +0530

    Enhance Hero and Button components: add margin to Hero and implement arrow display in Button

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,15 +1,18 @@
+import { ArrowRight } from 'lucide-react';
 import React from 'react'
 
 interface ButtonProps {
   btnText: string;
   type?: string;
   className?: string;
+  showArrow?: boolean;
 }
 
-const Button = ({btnText, type="primary", className=""}: ButtonProps) => {
+const Button = ({btnText, type="primary", className="", showArrow=false}: ButtonProps) => {
   return (
-    <div className={`${type === "primary" ? "bg-primary text-background hover:text-white hover:bg-transparent border border-primary" : "text-primary bg-transparent border-gradient-primary"} flex items-center justify-center px-5 rounded cursor-pointer transition font-bold ${className}`}>
+    <div className={`${type === "primary" ? "bg-primary text-background hover:text-white hover:bg-transparent border border-primary" : "text-primary bg-transparent border-gradient-primary"} flex items-center justify-center px-5 rounded cursor-pointer transition font-bold gap-2 ${className}`}>
       {btnText}
+      {showArrow && <ArrowRight size={18} />}
     </div>
   )
 }
